b328d38f6180673aca915fab542aaaae63b6157c
chrisjkellett <cjkellett@hotmail.co.uk>
2019-03-17 15:47:35 +0100

    semantic refactor of validation

--- app/src/containers/Form/validation/validation.js
+++ app/src/containers/Form/validation/validation.js
@@ -1,17 +1,28 @@
 const validation = {
+  failed: 0,
   checkField: function(obj, id){
-    const rules = obj[id].validation;
+    const field = obj[id];
+    const {validation} = field;
 
-    if(rules){
-      if(rules.required){
-        obj[id].valid = obj[id].value.trim() !== '';
-      }
-      if(rules.required_cb){
-        obj[id].valid = Object.keys(obj[id].value).some(item => obj[id].value[item])
-      }
+    if(validation){
+      if(validation.required) this.$isRequired(field);
+      if(validation.required_cb) this.$isRequiredCB(field);
     }
   
+    field.valid = this.$completeValidation();
     return obj;
+  },
+
+  $isRequired: function(field){
+    this.failed = field.value.trim() !== '' && this.failed++;
+  },
+
+  $isRequiredCB: function(field){
+    this.failed = Object.keys(field.value).some(item => field.value[item]) && this.failed++;
+  },
+  
+  $completeValidation: function(){
+    return this.failed === 0
   }
 }
 
