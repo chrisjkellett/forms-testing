7c5ad932f81d9b13cd4a632731426694d202246c
chrisjkellett <cjkellett@hotmail.co.uk>
2019-03-21 21:01:52 +0100

    added TextArea prop-types

--- app/src/forms/components/TextArea/TextArea.js
+++ app/src/forms/components/TextArea/TextArea.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {shape, func, array, string, bool, object} from 'prop-types';
 
 const TextArea = props => {
   return (
@@ -7,7 +8,15 @@ const TextArea = props => {
 }
 
 TextArea.propTypes = {
-
+  data: shape({
+    change: func.isRequired,
+    errors: array.isRequired,
+    id: string.isRequired,
+    touched: bool.isRequired,
+    type: string.isRequired,
+    validation: object.isRequired,
+    value: string.isRequired
+  })
 }
 
 export default TextArea;
---
+++ app/src/forms/components/TextArea/TextArea.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TextArea from './TextArea';
+import {shallow} from 'enzyme';
+
+describe('<TextArea />', () => {
+  const cmp = shallow(<TextArea data={null} />)
+  console.log(cmp.debug());
+  test('should ', () => {
+    
+  })
+})
